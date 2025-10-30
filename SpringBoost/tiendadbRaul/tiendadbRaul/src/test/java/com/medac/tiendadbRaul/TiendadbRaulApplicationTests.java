04d1bdb6eccecfe29acb943595d101d730a0080b
rcl0018 <rcl0018@alu.medac.es>
2025-10-30 08:44:33 +0100

    prueba de clase

---
+++ SpringBoost/tiendadbRaul/tiendadbRaul/src/test/java/com/medac/tiendadbRaul/TiendadbRaulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medac.tiendadbRaul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendadbRaulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
